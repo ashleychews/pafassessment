38e02b834750714a4e2351e3d74d3eca1347b188
ashleychews <84390323+ashleychews@users.noreply.github.com>
2024-01-26 16:33:54 +0800

    new file:   paf_assessment_template/bedandbreakfastapp/Dockerfile 	modified:   paf_assessment_template/bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/controllers/BnBController.java 	modified:   paf_assessment_template/data/task2.txt

--- paf_assessment_template/bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/controllers/BnBController.java
+++ paf_assessment_template/bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/controllers/BnBController.java
@@ -1,12 +1,9 @@
 package vttp2023.batch4.paf.assessment.controllers;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
